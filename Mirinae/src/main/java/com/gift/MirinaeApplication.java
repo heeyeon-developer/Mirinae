4b77b1025b27b41282d371bc4fa7ad943e6c1d1e
heeyeonkim <heeyeon3472@gmail.com>
2022-10-29 23:59:54 +0900

    Create Project

---
+++ Mirinae/src/main/java/com/gift/MirinaeApplication.java
@@ -0,0 +1,13 @@
+package com.gift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MirinaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MirinaeApplication.class, args);
+	}
+
+}
---
+++ Mirinae/src/test/java/com/gift/MirinaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MirinaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
